2fa4cfabe01ffb09a5cdc85f152161b397689598
AydynKorkin <aydynkorkin1@gmail.com>
2025-05-09 18:35:10 +0900

    feat: save_webhook_logs

--- app/Http/Controllers/WebhookController.php
+++ app/Http/Controllers/WebhookController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Services\AmoCrmAuthService;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
+use App\Models\WebhookLog;
 
 class WebhookController extends Controller
 {
@@ -12,7 +13,37 @@ class WebhookController extends Controller
     {
         $data = $request->all();
         Log::info(print_r($data, true));
-        $client = (new AmoCrmAuthService())->loadToken();
+
+        app(AmoCrmAuthService::class)->loadToken();
+
+        $this->processEvents($data, 'leads');
+        $this->processEvents($data, 'contacts');
+
         return response()->noContent();
+    }
+
+    protected function processEvents(array $data, string $type): void
+    {
+        if (!isset($data[$type])) {
+            return;
+        }
+
+        foreach (['add', 'update'] as $action) {
+            if (!empty($data[$type][$action])) {
+                foreach ($data[$type][$action] as $item) {
+                    $this->saveLog($data['account']['subdomain'] ?? null, $action, $item['name'] ?? null, $type);
+                }
+            }
+        }
+    }
+
+    protected function saveLog(?string $client, string $action, string $name, string $type): void
+    {
+        WebhookLog::query()->create([
+            'client_name' => $client,
+            'action' => $action,
+            'name' => $name,
+            'type' => $type
+        ]);
     }
 }
