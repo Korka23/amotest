9cd8761b557c91fb86ba1328b9798ded7068d47b
AydynKorkin <aydynkorkin1@gmail.com>
2025-05-09 16:24:42 +0900

    feat: test-access-token

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use AmoCRM\Client\LongLivedAccessToken;
+use AmoCRM\Exceptions\AmoCRMApiException;
+use AmoCRM\Exceptions\AmoCRMMissedTokenException;
+use AmoCRM\Exceptions\AmoCRMoAuthApiException;
 use AmoCRM\Exceptions\InvalidArgumentException;
 use App\Services\AmoCrmAuthService;
 use Illuminate\Http\Request;
@@ -24,6 +27,25 @@ class AuthController extends Controller
         $code = $request->get('code');
         (new AmoCrmAuthService())->exchangeCodeForToken($code);
         return 'Authorization successful.';
+    }
+
+    /**
+     * @throws InvalidArgumentException
+     * @throws AmoCRMMissedTokenException
+     */
+    public function accessToken()
+    {
+        $apiClient = new \AmoCRM\Client\AmoCRMApiClient();
+        $longLivedAccessToken = new LongLivedAccessToken(config('amocrm.access_token'));
+        $apiClient->setAccessToken($longLivedAccessToken)
+            ->setAccountBaseDomain(config('amocrm.base_domain'));
+
+        try {
+            $apiClient->contacts()->get();
+        } catch (AmoCRMMissedTokenException|AmoCRMoAuthApiException|AmoCRMApiException $e) {
+            return response(['error' => $e->getMessage()], 470);
+        }
+        return response()->noContent();
     }
 
 }
--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,9 @@ Route::group(['as' =>'auth', 'prefix' => 'auth'], function () {
    Route::get('redirect', [AuthController::class, 'redirect']);
    Route::get('callback', [AuthController::class, 'callback']);
 });
-Route::post('/webhook', [WebhookController::class, 'handle']);
+Route::post('webhook', [WebhookController::class, 'handle']);
+Route::get('test-access-token', [AuthController::class, 'accessToken']);
+
 Route::get('/', function () {
     return view('welcome');
 });
